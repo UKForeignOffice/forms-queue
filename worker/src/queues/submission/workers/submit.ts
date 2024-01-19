0c2c15a0c1acfa449f0527a03179c9c2863e4e9e
jen <jen@cautionyourblast.com>
2024-01-19 15:01:06 +0000

    logs

--- worker/src/queues/submission/workers/submit.ts
+++ worker/src/queues/submission/workers/submit.ts
@@ -11,6 +11,21 @@ const logger = pino();
 export const metadata = { queue, worker };
 const REQUEST_TIMEOUT = Number.parseInt(config.get<string>("Submission.requestTimeout"));
 logger.info(`REQUEST_TIMEOUT set to ${REQUEST_TIMEOUT}`);
+
+axios.interceptors.request.use((x) => {
+  // @ts-ignore
+  x.meta = x.meta || {};
+  // @ts-ignore
+  x.meta.requestStartedAt = new Date().getTime();
+  return x;
+});
+
+axios.interceptors.response.use((x) => {
+  // @ts-ignore
+  console.log(`Execution time for: ${x.config.url} - ${new Date().getTime() - x.config.meta.requestStartedAt} ms`);
+  return x;
+});
+
 /**
  * When a "submission" event is detected, this worker POSTs the data to `job.data.data.webhook_url`
  * The source of this event is the runner, after a user has submitted a form.
