420a7d159290540d5e421b8fd93e373304582677
jen <jen@cautionyourblast.com>
2024-01-19 15:32:06 +0000

    remove API

--- api/src/middlewares/index.ts
+++
@@ -1,7 +0,0 @@
-import { Application, json } from "express";
-import pino from "pino-http";
-
-export function initMiddleware(server: Application) {
-  server.use(pino());
-  server.use(json());
-}
--- api/src/routes/reference/jobIdRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { get, validate, post } from "./jobId";
-
-export const jobIdRouter = Router();
-
-jobIdRouter.get("/:jobId", get);
-
-jobIdRouter.post("/", validate, post);
