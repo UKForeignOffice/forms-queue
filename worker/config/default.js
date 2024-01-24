232b3d35e09f696a1e7731e43afc0b0c2a217f3c
jen <jen@cautionyourblast.com>
2024-01-24 15:35:53 +0000

    feat: allow archiveFailedInDays and deleteArchivedAfterDays to be configurable

--- worker/config/default.js
+++ worker/config/default.js
@@ -1,6 +1,8 @@
 module.exports = {
   Queue: {
     url: "postgres://user:root@localhost:5432/queue",
+    archiveFailedInDays: 30,
+    deleteArchivedAfterDays: 7,
   },
   Submission: {
     requestTimeout: 2000,
