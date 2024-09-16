c9c3f52a297a42ef299729fba87db75776413369
jen <jen@cautionyourblast.com>
2024-09-16 17:21:44 +0100

    feat: allow schema name to be configured via environment variable

--- worker/src/Consumer/getConsumer.ts
+++ worker/src/Consumer/getConsumer.ts
@@ -5,7 +5,6 @@ import pino from "pino";
 
 const URL = config.get<string>("Queue.url");
 const logger = pino().child({ method: "Consumer.create" });
-let consumer;
 
 const MINUTE_IN_S = 60;
 const HOUR_IN_S = MINUTE_IN_S * 60;
@@ -14,16 +13,23 @@ const DAY_IN_S = HOUR_IN_S * 24;
 const archiveFailedAfterDays = parseInt(config.get<string>("Queue.archiveFailedInDays"));
 const deleteAfterDays = parseInt(config.get<string>("Queue.deleteArchivedAfterDays"));
 
+const registeredConsumers: Record<string, PgBoss> = {};
+
 logger.info(`archiveFailedAfterDays: ${archiveFailedAfterDays}, deleteAfterDays: ${deleteAfterDays}`);
 
+type ConsumerOptions = {
+  schema?: string;
+};
 /**
  * Sets up database connection via PgBoss and creates an instance of a "consumer" (consumes the queue).
  */
-export async function create() {
+export async function create(options: ConsumerOptions) {
+  const { schema } = options;
   const boss = new PgBoss({
     connectionString: URL,
     archiveFailedAfterSeconds: archiveFailedAfterDays * DAY_IN_S,
     deleteAfterDays,
+    schema,
   });
 
   boss.on("error", (error) => {
@@ -47,13 +53,13 @@ export async function create() {
  * `getConsumer` should be used whenever an instance of a consumer is needed.
  * This is to prevent too many database connections from being opened unnecessarily.
  */
-export async function getConsumer() {
+export async function getConsumer(name: string) {
+  const consumer = registeredConsumers[name];
   try {
     if (!consumer) {
-      const boss = await create();
-      consumer = boss;
+      registeredConsumers[name] = await create({ schema: name });
     }
-    return consumer;
+    return registeredConsumers[name];
   } catch (e) {
     logger.error(e);
     process.exit(1);
